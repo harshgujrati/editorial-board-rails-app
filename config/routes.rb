6ab7db92bfdb9fae717ec8b891e609ed5175bf85
harshgujrati <harsh19may@gmail.com>
2020-07-22 17:26:16 +0530

    install rails_admin and remove notice from post index

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
 	
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   resources :posts
 	devise_for :users, controllers: {
 		sessions: 'users/sessions'
 	}
-	
+	root "posts#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
