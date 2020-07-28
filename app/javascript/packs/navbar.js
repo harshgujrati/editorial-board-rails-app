1adc4579ecc1b7725b7e9f9f6766753b599822f3
harshgujrati <harsh19may@gmail.com>
2020-07-28 19:16:36 +0530

    more frontend related stuff

--- app/javascript/packs/navbar.js
+++ app/javascript/packs/navbar.js
@@ -1,44 +1,32 @@
 // define all UI variable
 
-document.addEventListener('DOMContentLoaded', (event) => {
+window.addEventListener('DOMContentLoaded', (event) => {
     const navToggler = document.querySelector('.nav-toggler');
-	const navMenu = document.querySelector('.site-navbar ul');
-	const navLinks = document.querySelectorAll('.site-navbar a');
+const navMenu = document.querySelector('.site-navbar ul');
+const navLinks = document.querySelectorAll('.site-navbar a');
 
+// load all event listners
+allEventListners();
 
-	function navLinkClick() {
-	  if(navMenu.classList.contains('open')) {
-	    navToggler.click();
-	  }
-	}
-
-
-	function togglerClick() {
-	  navToggler.classList.toggle('toggler-open');
-	  navMenu.classList.toggle('open');
-	}
-	// functions of all event listners
-	function harsh() {
-	  // toggler icon click event
-	  
-	  	navToggler.addEventListener('click', togglerClick);
-	  
-	  
-	  // nav links click event
-	  
-	  	navLinks.forEach( elem => elem.addEventListener('click', navLinkClick));
-	  
-	  
-	}
-	// load all event listners
-	harsh();
-
-});
-
-
+// functions of all event listners
+function allEventListners() {
+  // toggler icon click event
+  navToggler.addEventListener('click', togglerClick);
+  // nav links click event
+  navLinks.forEach( elem => elem.addEventListener('click', navLinkClick));
+}
 
 // togglerClick function
-
+function togglerClick() {
+  navToggler.classList.toggle('toggler-open');
+  navMenu.classList.toggle('open');
+}
 
 // navLinkClick function
+function navLinkClick() {
+  if(navMenu.classList.contains('open')) {
+    navToggler.click();
+  }
+}
 
+});
