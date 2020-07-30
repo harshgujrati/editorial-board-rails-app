a68f0131b3f40e17e63dd7688bc7911b28bfd696
harshgujrati <harsh19may@gmail.com>
2020-07-30 12:41:32 +0530

    add limit to uploaded image

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,6 +9,6 @@ class HomeController < ApplicationController
   	@final_year_members = Member.where(year: 4)
   	@third_year_members = Member.where(year: 3)
   	@second_year_members = Member.where(year: 2)
-  	@first_year_members = Member.where(year: 1)
+  	
   end
 end
