ddae9a71934f98f5ceab09d24b762e780e39137b
harshgujrati <harsh19may@gmail.com>
2021-06-02 12:02:33 +0530

    add file upload to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,19 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  include ImageUploader::Attachment(:image)
+  
 
   has_many :posts, dependent: :destroy
   has_many :likes, dependent: :destroy
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_attached_file :avatar, styles: {
+    thumb: '100x100>',
+    square: '200x200#',
+    medium: '300x300>'
+  }
+  validates_attachment :avatar,
+                       content_type: { content_type: /\Aimage\/.*\z/ },
+                       size: { less_than: 1.megabyte }
 end
