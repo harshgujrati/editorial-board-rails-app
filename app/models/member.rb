2c2e379c9e8bc90399d99b12d091460d13d744cd
harshgujrati <harsh19may@gmail.com>
2021-06-05 16:57:53 +0530

    add default image

--- app/models/member.rb
+++ app/models/member.rb
@@ -4,7 +4,7 @@ class Member < ApplicationRecord
     thumb: '100x100>',
     square: '200x200#',
     medium: '300x300>'
-  }
+  }, default_url: "/images/:style/missingalumni.png"
   validates_attachment :avatar,
                        content_type: { content_type: /\Aimage\/.*\z/ },
                        size: { less_than: 1.megabyte }
